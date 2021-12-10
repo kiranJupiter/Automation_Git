bea0265737764b955c6ffbea23353a0153970231
kp <kp@DESKTOP-252HIRL>
2021-12-10 14:52:01 +0530

    Added First class to master branch. Here Our project itself Master branch , bcs there was no branch in git repository. Our project itself master

---
+++ Git_Automation/src/Git_Practice/Test.java
@@ -0,0 +1,10 @@
+package Git_Practice;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Thought easy but need to practice");
+	}
+
+}
