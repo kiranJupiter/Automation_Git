583302635c1620bb9dfc5e4b828335ca9e3585a0
kp <kp@DESKTOP-252HIRL>
2021-12-16 13:39:30 +0530

    updating

---
+++ Cucumber/src/main/java/Pure_java/Collecti2ons.java
@@ -0,0 +1,17 @@
+package Pure_java;
+
+import java.util.ArrayList;
+
+public class Collecti2ons {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		  ArrayList<String> l1=new ArrayList<String>();
+l1.add("Pipe");
+l1.add("todo");
+l1.add("1285");
+System.out.println(l1);
+
+	}
+
+}
